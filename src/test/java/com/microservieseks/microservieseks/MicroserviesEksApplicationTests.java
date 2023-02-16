02d098cf3b39680471769eb3d8a1365857530f2c
siva-007 <sivavignesh761@gmail,com>
2023-02-16 10:05:12 +0530

    spring application

---
+++ src/test/java/com/microservieseks/microservieseks/MicroserviesEksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservieseks.microservieseks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviesEksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
